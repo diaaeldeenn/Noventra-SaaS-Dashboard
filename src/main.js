540d4b100ddbb71078a566d1fc60761e62632361
Diaa Eldeen <diaaelseady@gmail.com>
2026-07-04 19:29:10 +0300

    .

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import app from "./app.controller.js";
 import connectionDB from "./DB/connectionDB.js";
-import { seedAdmin } from "./DB/seeds/admin.seed.js";
 
 connectionDB();
 const PORT = process.env.PORT || 5000;
